28affeba8561c6d4aa9d39d5bf68dedc6ac6d2eb
DevBilal17 <muhammadbilalmb03122@gmail.com>
2026-02-21 14:13:27 +0500

    Project Setup with Expo, Reanimated, NativeWind Done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./App.{js,jsx,ts,tsx}",
-    "./app/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}",
-    "./screens/**/*.{js,jsx,ts,tsx}"
+    "./app/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}"
   ],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
